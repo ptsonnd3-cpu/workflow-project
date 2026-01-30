48ad732609f545c8b69f76470f70477cb2860da4
DESKTOP-N52HT8D\mrson <taisonway@gmai.com>
2026-01-30 09:08:57 +0700

    Add env REACT_APP_API_URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 import 'bpmn-js/dist/assets/diagram-js.css';
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = process.env.REACT_APP_API_URL;//'http://localhost:5000';
 
 // Create Query Client with optimized configuration
 const queryClient = new QueryClient({
--- frontend/src/WorkflowManagement.js
+++ frontend/src/WorkflowManagement.js
@@ -4,7 +4,7 @@ import './App.css';
 import 'bpmn-js/dist/assets/diagram-js.css';
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = process.env.REACT_APP_API_URL;//'http://localhost:5000';
 
 // Utility function for safe BPMN canvas zooming
 const safeBpmnZoom = (canvas, zoomType = 'fit-viewport', fallbackZoom = 1.0) => {
